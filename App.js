8bf79ed42738caf7841c4cb2c650be9d34734ab4
Becky <bjentwistle@gmail.com>
2024-01-27 19:06:23 +0000

    first commit of mobile app

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  let x = 1;
+  console.log("App executed");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>This is my first mobile app</Text>
+      <Text style={styles.text}>Hello World from Becky!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#61dafb",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    marginTop: 16,
+    padding: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 25,
+    fontWeight: "bold",
   },
 });
